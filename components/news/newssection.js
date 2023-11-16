3bb1d2b656d9a42c15d5f994ff8229ce0f2bbd50
nevermore <kl.nevermore@gmail.com>
2023-11-16 14:26:18 +0800

    feat:r

--- components/news/newssection.js
+++ components/news/newssection.js
@@ -17,6 +17,7 @@ export default function NewsSection(props) {
         } else {
             props.setNewsActive(true);
         }
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
 
     const onNewsClose = () => {
