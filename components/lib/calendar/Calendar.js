1630623fba82f18ee02e0e51401037514c4dbb1c
Liu7i <liu67iq@gmail.com>
2023-12-12 16:02:42 +0800

    修改方法名称

--- components/lib/calendar/Calendar.js
+++ components/lib/calendar/Calendar.js
@@ -3606,7 +3606,7 @@ export const Calendar = React.memo(
             return null;
         };
 
-        const createUEXTimePicker = () => {
+        const createTimeTemplate = () => {
             if (props.timeTemplate && (props.showTime || props.timeOnly) && currentView === 'date') {
                 return props.timeTemplate({
                     selectHour,
@@ -3818,7 +3818,7 @@ export const Calendar = React.memo(
 
         const content = createContent();
         const datePicker = createDatePicker();
-        const timePicker = createUEXTimePicker();
+        const timePicker = createTimeTemplate();
         const buttonBar = createButtonBar();
         const footer = createFooter();
         const monthPicker = createMonthPicker();
